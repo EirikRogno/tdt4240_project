de98ee4b0ddc6d68bf3f0c58611c849929722cc5
ninarg <ninagraneggen@yahoo.no>
2017-03-21 22:04:01 +0100

    Drawing functionality added

--- app/src/main/java/com/example/eirik/tdt4240_project/drawing/DrawingActivity.java
+++ app/src/main/java/com/example/eirik/tdt4240_project/drawing/DrawingActivity.java
@@ -1,15 +1,20 @@
 package com.example.eirik.tdt4240_project.drawing;
 
+import android.app.Activity;
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.eirik.tdt4240_project.R;
 
-public class DrawingActivity extends AppCompatActivity {
+public class DrawingActivity extends Activity {
+
+    private DrawingController drawingController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawing);
+        this.drawingController = (DrawingController)findViewById(R.id.draw);
     }
 }
--- app/src/main/java/com/example/eirik/tdt4240_project/drawing/DrawingController.java
+++ app/src/main/java/com/example/eirik/tdt4240_project/drawing/DrawingController.java
@@ -1,4 +1,52 @@
 package com.example.eirik.tdt4240_project.drawing;
 
-public class DrawingController {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawingController extends View {
+
+    private Paint paint;
+    private Path path;
+    private float x;
+    private float y;
+
+    public DrawingController(Context context, AttributeSet as){
+        super(context, as);
+        paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        path = new Path(); // follows the path of drawing
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        canvas.drawPath(path,paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // touch coordinates
+        x = event.getX();
+        y = event.getY();
+
+        // update path
+        if (event.getAction() == 0) { // ACTION_DOWN
+            path.moveTo(x,y);
+        } else if (event.getAction() == 2) {// ACTION_MOVE
+            path.lineTo(x,y);
+        } else if (event.getAction() == 1) {// ACTION_UP
+            path.lineTo(x,y);
+        }
+
+        //force view to draw
+        invalidate();
+
+        return true;
+    }
 }
