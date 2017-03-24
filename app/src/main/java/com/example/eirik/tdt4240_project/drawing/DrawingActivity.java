01a319d5a33eccb6173c5f1ece428c3271b08915
ninarg <ninagraneggen@yahoo.no>
2017-03-24 16:59:39 +0100

    Added send button

--- app/src/main/java/com/example/eirik/tdt4240_project/drawing/DrawingActivity.java
+++ app/src/main/java/com/example/eirik/tdt4240_project/drawing/DrawingActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.eirik.tdt4240_project.R;
@@ -11,19 +13,32 @@ import com.example.eirik.tdt4240_project.R;
 public class DrawingActivity extends Activity {
 
     private DrawingController drawingController;
-    private TextView word;
+    private TextView wordField;
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawing);
         this.drawingController = (DrawingController)findViewById(R.id.draw);
-        this.word = (TextView)findViewById(R.id.word);
+        this.wordField = (TextView)findViewById(R.id.word);
+
+        this.sendButton = (Button)findViewById(R.id.send);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                stopDrawingActivity();
+            }
+        });
+
         startDrawingActivity();
     }
 
     private void startDrawingActivity() {
-        // get a word from server?
-        word.setText("Banan");
+        // get a word from server
+        wordField.setText("Banan");
+    }
+
+    private void stopDrawingActivity() {
+        // send drawing to server
     }
 }
