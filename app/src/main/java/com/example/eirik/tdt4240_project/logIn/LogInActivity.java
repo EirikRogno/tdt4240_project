c5f06932da90348387ff7a47008806374451ed2c
arnagl <arna.glavovic@gmail.com>
2017-03-27 18:13:51 +0200

    edited main menu view and the logIn view

--- app/src/main/java/com/example/eirik/tdt4240_project/logIn/LogInActivity.java
+++ app/src/main/java/com/example/eirik/tdt4240_project/logIn/LogInActivity.java
@@ -1,17 +1,36 @@
 package com.example.eirik.tdt4240_project.logIn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.eirik.tdt4240_project.R;
+import com.example.eirik.tdt4240_project.mainMenu.MainMenuActivity;
+
 
 public class LogInActivity extends AppCompatActivity {
 
-    //login button
-    //username
+    /*
+    public void inputButtonOnClick(View v){
+        if(v.getId() == R.id.logInButton){
+            Intent i = new Intent(LogInActivity.this, MainMenuActivity.class);
+            startActivity(i);
+        }
+    }
+    */
 
-    public Boolean onActionLoginButton(){
-        return null;
+    Button logInButton = (Button) findViewById(R.id.btnLogIn);
+
+    public void inputButtionOnClick(View v){
+        if(v.getId() == R.id.btnLogIn){
+            logInButton.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    startActivity(new Intent(LogInActivity.this, MainMenuActivity.class));
+                }
+            });
+        }
     }
 
     @Override
