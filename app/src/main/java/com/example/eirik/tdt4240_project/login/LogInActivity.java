45638b8dcaaabb3888907835ad48297a74a4b1db
Sindre Skåland brun <sindre.brun@outlook.com>
2017-04-21 12:17:31 +0200

    Pushing from dashboard now working

--- app/src/main/java/com/example/eirik/tdt4240_project/login/LogInActivity.java
+++ app/src/main/java/com/example/eirik/tdt4240_project/login/LogInActivity.java
@@ -2,6 +2,7 @@ package com.example.eirik.tdt4240_project.login;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,9 +13,11 @@ import android.widget.EditText;
 
 import com.example.eirik.tdt4240_project.R;
 import com.example.eirik.tdt4240_project.mainmenu.MainMenuActivity;
+import com.pushwoosh.fragment.PushEventListener;
+import com.pushwoosh.fragment.PushFragment;
 
 
-public class LogInActivity extends AppCompatActivity {
+public class LogInActivity extends FragmentActivity implements PushEventListener {
 
     EditText usernameInput;
     LogInController controller = new LogInController();
@@ -52,5 +55,50 @@ public class LogInActivity extends AppCompatActivity {
 
         usernameInput = (EditText)findViewById(R.id.txtUsername);
 
+        //Init Pushwoosh fragment
+        PushFragment.init(this);
+
     }
+
+    @Override
+    public void onNewIntent(Intent intent)
+    {
+        super.onNewIntent(intent);
+
+        //Check if we've got new intent with a push notification
+        PushFragment.onNewIntent(this, intent);
+    }
+
+    private final String TAG = "pushwoosh";
+
+    @Override
+    public void doOnRegistered(String registrationId)
+    {
+        Log.i(TAG, "Registered for pushes: " + registrationId);
+    }
+
+    @Override
+    public void doOnRegisteredError(String errorId)
+    {
+        Log.e(TAG, "Failed to register for pushes: " + errorId);
+    }
+
+    @Override
+    public void doOnMessageReceive(String message)
+    {
+        Log.i(TAG, "Notification opened: " + message);
+    }
+
+    @Override
+    public void doOnUnregistered(final String message)
+    {
+        Log.i(TAG, "Unregistered from pushes: " + message);
+    }
+
+    @Override
+    public void doOnUnregisteredError(String errorId)
+    {
+        Log.e(TAG, "Failed to unregister from pushes: " + errorId);
+    }
+
 }
